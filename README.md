926c238ab056243b1f545d69fab419572d2978f9
Simon Ansell <siansell@gmail.com>
2019-01-27 21:16:14 +0000

    basic layout

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+Requirement
+Your task is to build a single page application that will play the role of a phone book.
+
+
+## Requirements
+
+- use this api to get the data you need: http://www.mocky.io/v2/581335f71000004204abaf83
+
+- the app needs to allow the user to create, edit or delete a contact (in a stateless mode, not permanently)
+
+- you need to display all the contacts and add basic filters for searching and sorting
+
+please describe in a readme file how to run your application
+
+
+We strongly recommend you use frameworks to solve the challenge if you added frameworks to your hackajob profile.
+
+---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header, Segment, Message, Table, Button, Icon, Input, List } from 'semantic-ui-react'
+import axios from 'axios'
+
+import 'semantic-ui-css/semantic.min.css'
 
 class App extends Component {
+  state = {
+    contacts: [],
+    error: false,
+    isLoaded: false,
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('http://www.mocky.io/v2/581335f71000004204abaf83')
+      const { contacts } = response.data
+      this.setState({
+        contacts,
+        isLoaded: true,
+      })
+    } catch (e) {
+      this.setState({
+        error: e.message,
+        isLoaded: true,
+      })
+    }
+
+  }
+
   render() {
+    const { contacts, error, isLoaded } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container style={{ margin: '1rem' }}>
+        <Header as="h1">Phonebook</Header>
+        <List bulleted>
+          <List.Item>By <a href="https://twitter.com/simon_ansell" target="_blank" rel="noopener noreferrer">Simon Ansell</a> for <a href="https://www.hackajob.co" target="_blank" rel="noopener noreferrer">hackajob</a></List.Item>
+          <List.Item>Type in the inputs to filter</List.Item>
+          <List.Item>Mouseover a contact to reveal edit and delete actions</List.Item>
+        </List>
+        <Segment loading={!isLoaded}>
+          {error && <Message error>Something went wrong: {error}</Message>}
+          {!error && contacts && (
+            <>
+              <div style={{ display:'flex', width: '100%' }}>
+                <Header as="h2" style={{ flex: 1 }}>{contacts.length} contacts</Header>
+                <Button primary icon="plus" labelPosition="right" content="New contact" />
+              </div>
+              {contacts.length > 0 && (
+                <Table selectable size="small" compact basic="very">
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell width={3}><Input size="small" placeholder="name" /><Icon name="sort" /></Table.HeaderCell>
+                      <Table.HeaderCell width={3}><Input size="small" placeholder="phone_number" /><Icon name="sort" /></Table.HeaderCell>
+                      <Table.HeaderCell width={8}><Input size="small" placeholder="address" /><Icon name="sort" /></Table.HeaderCell>
+                      <Table.HeaderCell width={2}>&nbsp;</Table.HeaderCell>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                    {contacts.map((c, i) => (
+                      <Table.Row key={i}>
+                        <Table.Cell>{c.name}</Table.Cell>
+                        <Table.Cell>{c.phone_number}</Table.Cell>
+                        <Table.Cell>{c.address}</Table.Cell>
+                        <Table.Cell>
+                          <Button basic size="mini" color="blue">edit</Button>
+                          <Button basic size="mini" color="red">delete</Button>
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                  </Table.Body>
+                </Table>
+              )}
+            </>
+          )}
+        </Segment>
+      </Container>
     );
   }
 }
